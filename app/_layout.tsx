810ce4d193fbf6217a48dd824f9699462f9bf622
jungheol <jungheol@naver.com>
2025-02-17 10:55:35 +0900

    feat: 상단 네비게이션 바 숨기기

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,9 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex:1 }}>
-      <Stack />
+      <Stack screenOptions={{ headerShown: false }}> 
+      {/* 🔥 모든 화면에서 헤더 숨기기 */}
+      </Stack>
     </GestureHandlerRootView>
   );
 }
